dc3a6e91be9f0537347cb7a98bf1a5cccfd6daf1
Vetingof <vetingof@gmail.com>
2023-03-22 21:00:55 +0200

    Initial code Calculator

---
+++ src/telran/numbers/Calculator.java
@@ -0,0 +1,16 @@
+package telran.numbers;
+
+public class Calculator {
+	static public int multiply(int op1, int op2) {
+		return op1 * op2;
+	}
+	static public int sum(int op1, int op2) {
+		return op1 + op2;
+	}
+	static public int divide (int op1, int op2) {
+		return op1 / op2;
+	}
+	static public int remainder (int op1, int op2) {
+		return op1 % op2;
+	}
+}
